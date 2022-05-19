d7108dd16e6396a605bd02cd29fb85537fdf7456
changchang4825 <dlckdals4825@gmail.com>
2022-05-20 01:50:28 +0900

    init battle stage

---
+++ battle.js
@@ -0,0 +1,22 @@
+let skill_point1;
+let skill_point2;
+let skill_point3;
+let skill_point4;
+
+let enemy_skill_point1;
+let enemy_skill_point2;
+let enemy_skill_point3;
+let enemy_skill_point4;
+
+document.getElementById("skill1").addEventListener("click", () => {
+
+})
+document.getElementById("skill2").addEventListener("click", () => {
+
+})
+document.getElementById("skill3").addEventListener("click", () => {
+
+})
+document.getElementById("skill4").addEventListener("click", () => {
+
+})
