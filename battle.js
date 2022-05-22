bd5362ce2eb1065b8a96a55af511c0ad30d9084c
changchang4825 <dlckdals4825@gmail.com>
2022-05-22 20:40:07 +0900

    modified field

--- battle.js
+++ battle.js
@@ -1,30 +1,30 @@
 let myPoketmon1 = {
-    name: "Picachu",
+    name: "Yelly",
     hp: 555,
-    skill_name: ["baekmanbolt", "jeongwang", "hpUp", "ironTale"],
+    skill_name: ["Lightening", "Throw", "HpUp", "IronTale"],
     skill_damage: [-100, -20, 333, -27],
-    skill_point: [10, 10, 20, 10],
+    skill_point: [5, 10, 20, 10],
 };
 let myPoketmon2 = {
-    name: "Charmander",
+    name: "Prom",
     hp: 505,
-    skill_name: ["flame", "firewall", "rapidfire", "hpUp"],
-    skill_damage: [-100, -20, -200, 27],
-    skill_point: [15, 20, 5, 0],
+    skill_name: ["Punch", "SuperPunch", "Shoute", "hpUp"],
+    skill_damage: [-77, -200, -200, 50],
+    skill_point: [15, 6, 3, 5],
 };
 let myPoketmon3 = {
-    name: "Squirtle",
+    name: "Kochstrasse",
     hp: 632,
-    skill_name: ["waterpark", "megadrop", "hpUp", "hydropump"],
-    skill_damage: [-100, -20, 333, -77],
-    skill_point: [5, 15, 5, 10],
+    skill_name: ["Booo", "HatTrick", "HpUp", "Whipping"],
+    skill_damage: [-33, -250, 333, -77],
+    skill_point: [25, 3, 7, 15],
 };
 let myPoketmon4 = {
-    name: "Bulbasaur",
+    name: "Cottonji",
     hp: 700,
-    skill_name: ["leaftornado", "blooming", "hpUp", "spreadSeed"],
-    skill_damage: [-100, -70, 333, -30],
-    skill_point: [10, 5, 20, 0],
+    skill_name: ["Cavityfy", "CottonBapping", "HpUp", "Stare"],
+    skill_damage: [-100, -33, 600, -30],
+    skill_point: [10, 15, 20, 10],
 };
 
 let myPoketArr = {};
@@ -35,81 +35,81 @@ myPoketArr[3] =  myPoketmon4;
 
 // stage 1
 let wildPoketmon1 = {
-    name: "Pidgey",
+    name: "Greepy",
     hp: 150,
-    skill_name: ["momtong", "wind", "hpUp", "JJogi"],
-    skill_damage: [-15, 0, 10, -15],
-    skill_point: [10, 5, 20, 10],
+    skill_name: ["Bite", "Headbanging", "HpUp", "Stare"],
+    skill_damage: [-15, -10, 100, -15],
+    skill_point: [100, 100, 100, 100],
 };
 
 
 // stage 2
 let wildPoketmon2_1 = {
-    name: "Nidoking",
+    name: "Happydancer",
     hp: 400,
-    skill_name: ["poison", "struggle", "hpUp", "Sheer"],
-    skill_damage: [-20, 0, 50, -37],
-    skill_point: [15, 15, 5, 15],
+    skill_name: ["Dance", "Shear", "HpUp", "HeadSpin"],
+    skill_damage: [-20, -20, 77, -37],
+    skill_point: [100, 100, 100, 100],
 };
 let wildPoketmon2_2 = {
-    name: "Nidoqueen",
+    name: "Hellohola",
     hp: 300,
-    skill_name: ["poison", "struggle", "hpUp", "Sheer"],
-    skill_damage: [-20, 0, 50, -37],
-    skill_point: [15, 15, 5, 15],
+    skill_name: ["Greeting", "Handshaking", "HpUp", "Spiting"],
+    skill_damage: [-5, -20, 177, -15],
+    skill_point: [100, 100, 100, 100],
 };
 
 // stage 3
 let wildPoketmon3_1 = {
-    name: "Yadon",
+    name: "Picky",
     hp: 400,
-    skill_name: ["ya", "do", "on", "hpUp"],
-    skill_damage: [-10, -15, -20, 50],
-    skill_point: [15, 15, 15, 5],
+    skill_name: ["FirstTeeth", "SecondTeeth", "ThirdTeeth", "FourthTeeth"],
+    skill_damage: [-10, -20, -35, -55],
+    skill_point: [100, 100, 100, 100],
 };
 let wildPoketmon3_2 = {
-    name: "Yadoran",
+    name: "MonsterJelly",
     hp: 500,
-    skill_name: ["ya", "do", "ran", "hpUp"], 
-    kill_damage: [-10, -15, -20, 100],
-    skill_point: [15, 15, 15, 5],
+    skill_name: ["Jellyfy", "Rage", "Inrage", "hpUp"], 
+    skill_damage: [-100, -120, -210, 100],
+    skill_point: [100, 100, 100, 100],
 };
 let wildPoketmon3_3 = {
-    name: "MegaYadoran  ",
+    name: "Unluckyangel",
     hp: 1000,
-    skill_name: ["mega", "ya", "do", "ran"],
-    kill_damage: [-10, -15, -20, -200],
-    skill_point: [15, 15, 15, 5],
+    skill_name: ["Somersault", "LoveDive", "Stare", "HpUp"],
+    skill_damage: [-100, -150, -20, 500],
+    skill_point: [100, 100, 100, 100],
 };
 
 // stage 4
 let wildPoketmon4_1 = {
-    name: "JamManBo",
-    hp: 1500,
-    skill_name: ["Zzz", "Bite", "Momtong", "Hapum"],
-    skill_damage: [100, -100, -200, -15],
-    skill_point: [10, 10, 5, 25],
+    name: "Sleepy",
+    hp: 3000,
+    skill_name: ["Zzz", "CocoNenne", "HpUp", "Hapum"],
+    skill_damage: [1000, 700, 800, -200],
+    skill_point: [100, 100, 100, 100],
 };
 let wildPoketmon4_2 = {
-    name: "Majayong",
-    hp: 700,
-    skill_name: ["hpUp", "Mirror", "Momtong", "Majayong"],
-    skill_damage: [300, 0, -5, -30],
-    skill_point: [10, 10, 20, 15],
+    name: "Tboy",
+    hp: 1300,
+    skill_name: ["HpUp", "Mirror", "Meteo", "Hacking"],
+    skill_damage: [300, 10, -444, -139],
+    skill_point: [100, 100, 100, 100],
 };
 let wildPoketmon4_3 = {
-    name: "Happynurse",
-    hp: 800,
-    skill_name: ["hpUp1", "hpUp2", "hpUp3", "melody"],
-    skill_damage: [150, 175, 200, -37],
-    skill_point: [7, 7, 7, 35],
+    name: "Cornelio",
+    hp: 1800,
+    skill_name: ["ThrowPickle", "Crying", "HpUp", "Boxing"],
+    skill_damage: [-150, -175, 200, -78],
+    skill_point: [100, 100, 100, 100],
 };
 let wildPoketmon4_4 = {
-    name: "Dialga",
+    name: "Demon",
     hp: 1717,
-    skill_name: ["breath", "mist", "flying", "hpUp"],
-    skill_damage: [-100, -75, -66, 757],
-    skill_point: [15, 10, 10, 7],
+    skill_name: ["Curse", "Spawn", "FireRain", "Psychic"],
+    skill_damage: [-200, -33, -66, -666],
+    skill_point: [100, 100, 100, 100],
 };
 
 let wildPoketArr = {};
@@ -131,10 +131,60 @@ wildPoketArr[3][3] = wildPoketmon4_4;
 
 let my_monster;
 let my_monster_state;
+let my_monster_index;
+let is_alive;
 let enemy_monster;
 let enemy_monster_state;
+let current_stage;
+let current_enemy = 0;
 
 function sleep(ms) { return new Promise((resolve) => setTimeout(resolve, ms)) }
+
+function my_change() {
+    let num = Math.floor(Math.random() * 4);
+    for (let i = 0; !is_alive[num]; i++) {
+        if (i >= 3) {
+            location.href = "index.html";
+            return;
+        }
+        num = (num + 1) % 4;
+    }
+    my_monster = JSON.parse(JSON.stringify(myPoketArr[num]));
+    my_monster_state = JSON.parse(JSON.stringify(my_monster));
+    my_monster_index = num;
+    document.getElementById("my_name").innerHTML = my_monster.name;
+    document.getElementById("my_hp").innerHTML = my_monster_state.hp + "/" + my_monster.hp;
+    document.getElementById("my_image").src = "img/" + my_monster.name.toLowerCase() + ".gif";
+    document.getElementById("skill_name1").innerHTML = my_monster.skill_name[0];
+    document.getElementById("skill_name2").innerHTML = my_monster.skill_name[1];
+    document.getElementById("skill_name3").innerHTML = my_monster.skill_name[2];
+    document.getElementById("skill_name4").innerHTML = my_monster.skill_name[3];
+    document.getElementById("skill_point1").innerHTML = "(" + my_monster_state.skill_point[0] + "/" + my_monster.skill_point[0] + ")";
+    document.getElementById("skill_point2").innerHTML = "(" + my_monster_state.skill_point[1] + "/" + my_monster.skill_point[1] + ")";
+    document.getElementById("skill_point3").innerHTML = "(" + my_monster_state.skill_point[2] + "/" + my_monster.skill_point[2] + ")";
+    document.getElementById("skill_point4").innerHTML = "(" + my_monster_state.skill_point[3] + "/" + my_monster.skill_point[3] + ")";
+    document.getElementById("battle_log").innerHTML = "What to do...?";
+}
+
+function enemy_change() {
+    if (current_stage == 1) {
+        enemy_monster = JSON.parse(JSON.stringify(wildPoketArr[0]));
+    }
+    else if (current_stage == 2) {
+        enemy_monster = JSON.parse(JSON.stringify(wildPoketArr[1][current_enemy]));
+    }
+    else if (current_stage == 3) {
+        enemy_monster = JSON.parse(JSON.stringify(wildPoketArr[2][current_enemy]));
+    }
+    else if (current_stage == 4) {
+        enemy_monster = JSON.parse(JSON.stringify(wildPoketArr[3][current_enemy]));
+    }
+    enemy_monster_state = JSON.parse(JSON.stringify(enemy_monster));
+    document.getElementById("enemy_name").innerHTML = enemy_monster.name;
+    document.getElementById("enemy_hp").innerHTML = enemy_monster_state.hp + "/" + enemy_monster.hp;
+    document.getElementById("enemy_image").src = "img/" + enemy_monster.name.toLowerCase() + ".gif";
+    document.getElementById("battle_log").innerHTML = "What to do...?";
+}
 
 function my_attack(skill_num) {
     if (my_monster_state.skill_point[skill_num] > 0) {
@@ -144,10 +194,18 @@ function my_attack(skill_num) {
             document.getElementById("battle_log").innerHTML = my_monster.skill_name[skill_num] + "! " + enemy_monster.name + " was taken " + -my_monster_state.skill_damage[skill_num] + " damage!";
             if (enemy_monster_state.hp <= 0) {
                 enemy_monster_state.hp = 0;
+                document.getElementById("enemy_hp").innerHTML = enemy_monster_state.hp + "/" + enemy_monster.hp;
                 document.getElementById("battle_log").innerHTML += " <strong>Win!!!</strong>";
-                if (Number(window.localStorage.getItem("current_stage")) > Number(window.localStorage.getItem("last_stage"))) {
+                if (current_enemy < current_stage - 1) {
+                    current_enemy++;
+                    sleep(2000).then(() => enemy_change());
+                    return;
+                }
+                else if (Number(window.localStorage.getItem("current_stage")) > Number(window.localStorage.getItem("last_stage"))) {
                     window.localStorage.setItem("last_stage", window.localStorage.getItem("current_stage"))
                 }
+                sleep(2000).then(() => { location.href = 'index.html'; });
+                return;
             }
             document.getElementById("enemy_hp").innerHTML = enemy_monster_state.hp + "/" + enemy_monster.hp;
         }
@@ -161,7 +219,7 @@ function my_attack(skill_num) {
         else {
             document.getElementById("battle_log").innerHTML = "Nothing happend..."
         }
-        sleep(3000).then(() => enemy_attack());
+        sleep(2000).then(() => enemy_attack());
     }
     else {
         document.getElementById("battle_log").innerHTML = "<strong>Cannot use this skill !!!</strong>"
@@ -171,7 +229,9 @@ function my_attack(skill_num) {
 function enemy_attack() {
     let skill_num = Math.floor(Math.random() * 4);
     for (let i = 0; enemy_monster_state.hp === enemy_monster.hp && enemy_monster_state.skill_damage[skill_num] > 0; i++) {
-        if (i >= 3) return;
+        if (i >= 3) {
+            return;
+        }
         skill_num = (skill_num + 1) % 4;
     }
     if (enemy_monster_state.skill_damage[skill_num] < 0) {
@@ -179,7 +239,11 @@ function enemy_attack() {
         document.getElementById("battle_log").innerHTML = enemy_monster.name + "'s " + enemy_monster.skill_name[skill_num] + "! " + my_monster.name + " was taken " + -enemy_monster_state.skill_damage[skill_num] + " damage!";
         if (my_monster_state.hp <= 0) {
             my_monster_state.hp = 0;
+            document.getElementById("my_hp").innerHTML = my_monster_state.hp + "/" + my_monster.hp;
             document.getElementById("battle_log").innerHTML += " <strong>Lose...</strong>";
+            is_alive[my_monster_index] = 0;
+            sleep(2000).then(() => my_change());
+            return;
         }
         document.getElementById("my_hp").innerHTML = my_monster_state.hp + "/" + my_monster.hp;
     }
@@ -190,41 +254,13 @@ function enemy_attack() {
         document.getElementById("battle_log").innerHTML = enemy_monster.name + "'s " + heal + " HP healed!!!";
         document.getElementById("enemy_hp").innerHTML = enemy_monster_state.hp + "/" + enemy_monster.hp;
     }
-    else {
-        //////////////////
-    }
 }
 
 window.onload = () => {
-    let current_stage = window.localStorage.getItem("current_stage");
-    my_monster = JSON.parse(JSON.stringify(myPoketArr[Math.floor(Math.random() * 4)]));
-    my_monster_state = JSON.parse(JSON.stringify(my_monster));
-    if (current_stage == 1) {
-        enemy_monster = JSON.parse(JSON.stringify(wildPoketArr[0]));
-    }
-    else if (current_stage == 2) {
-        enemy_monster = JSON.parse(JSON.stringify(wildPoketArr[1][Math.floor(Math.random() * 2)]));
-    }
-    else if (current_stage == 3) {
-        enemy_monster = JSON.parse(JSON.stringify(wildPoketArr[2][Math.floor(Math.random() * 3)]));
-    }
-    else if (current_stage == 4) {
-        enemy_monster = JSON.parse(JSON.stringify(wildPoketArr[3][Math.floor(Math.random() * 4)]));
-    }
-    enemy_monster_state = JSON.parse(JSON.stringify(enemy_monster));
-
-    document.getElementById("my_name").innerHTML = my_monster.name;
-    document.getElementById("my_hp").innerHTML = my_monster_state.hp + "/" + my_monster.hp;
-    document.getElementById("enemy_name").innerHTML = enemy_monster.name;
-    document.getElementById("enemy_hp").innerHTML = enemy_monster_state.hp + "/" + enemy_monster.hp;
-    document.getElementById("skill_name1").innerHTML = my_monster.skill_name[0];
-    document.getElementById("skill_name2").innerHTML = my_monster.skill_name[1];
-    document.getElementById("skill_name3").innerHTML = my_monster.skill_name[2];
-    document.getElementById("skill_name4").innerHTML = my_monster.skill_name[3];
-    document.getElementById("skill_point1").innerHTML = "(" + my_monster_state.skill_point[0] + "/" + my_monster.skill_point[0] + ")";
-    document.getElementById("skill_point2").innerHTML = "(" + my_monster_state.skill_point[1] + "/" + my_monster.skill_point[1] + ")";
-    document.getElementById("skill_point3").innerHTML = "(" + my_monster_state.skill_point[2] + "/" + my_monster.skill_point[2] + ")";
-    document.getElementById("skill_point4").innerHTML = "(" + my_monster_state.skill_point[3] + "/" + my_monster.skill_point[3] + ")";
+    is_alive = [1, 1, 1, 1];
+    current_stage = window.localStorage.getItem("current_stage");
+    my_change();
+    enemy_change();
 }
 
 document.getElementById("skill1").addEventListener("click", () => {
--- monsterInfo.js
+++ monsterInfo.js
@@ -1,130 +1,116 @@
-let myPoketmon1 = {
-    name: "Picachu",
-    hp: 555,
-    skill_name: ["baekmanbolt", "jeongwang", "hpUp", "ironTale"],
-    skill_damage: [-100, -20, 333, -27],
-    skill_point: [10, 10, 20, 10],
-};
-let myPoketmon2 = {
-    name: "Charmander",
-    hp: 505,
-    skill_name: ["flame", "firewall", "rapidfire", "hpUp"],
-    skill_damage: [-100, -20, -200, 27],
-    skill_point: [15, 20, 5, 0],
-};
-let myPoketmon3 = {
-    name: "Squirtle",
-    hp: 632,
-    skill_name: ["waterpark", "megadrop", "hpUp", "hydropump"],
-    skill_damage: [-100, -20, 333, -77],
-    skill_point: [5, 15, 5, 10],
-};
-let myPoketmon4 = {
-    name: "Bulbasaur",
-    hp: 700,
-    skill_name: ["leaftornado", "blooming", "hpUp", "spreadSeed"],
-    skill_damage: [-100, -70, 333, -30],
-    skill_point: [10, 5, 20, 0],
-};
-
-let myPoketArr = {};
+// myPoketmon중에 하나 선택해서 시작해서 스테이지 해금할때마다 하나씩 추가(round robin)
+var myPoketArr = {};
 myPoketArr[0] =  myPoketmon1;
 myPoketArr[1] =  myPoketmon2;
 myPoketArr[2] =  myPoketmon3;
 myPoketArr[3] =  myPoketmon4;
+let myPoketmon1 = {
+    name: "yelly",
+    hp: 555,
+    skill_name: ["Lightening", "Throw", "HpUp", "IronTale"],
+    skill_damage: [-100, -20, 333, -27],
+    skill_point: [5, 10, 20, 10],
+};
+let myPoketmon2 = {
+    name: "prom",
+    hp: 505,
+    skill_name: ["Punch", "SuperPunch", "Shoute", "hpUp"],
+    skill_damage: [-77, -200, -200, 50],
+    skill_point: [15, 6, 3, 5],
+};
+let myPoketmon3 = {
+    name: "kochstrasse",
+    hp: 632,
+    skill_name: ["Booo", "HatTrick", "HpUp", "Whipping"],
+    skill_damage: [-33, -250, 333, -77],
+    skill_point: [25, 3, 7, 15],
+};
+let myPoketmon4 = {
+    name: "cottonji",
+    hp: 700,
+    skill_name: ["Cavityfy", "CottonBapping", "HpUp", "Stare"],
+    skill_damage: [-100, -33, 600, -30],
+    skill_point: [10, 15, 20, 10],
+};
+
+
+
 
 // stage 1
 let wildPoketmon1 = {
-    name: "Pidgey",
+    name: "greepy",
     hp: 150,
-    skill_name: ["momtong", "wind", "hpUp", "JJogi"],
-    skill_damage: [-15, 0, 10, -15],
-    skill_point: [10, 5, 20, 10],
+    skill_name: ["Bite", "Headbanging", "HpUp", "Stare"],
+    skill_damage: [-15, -10, 100, -15],
+    skill_point: [100, 100, 100, 100],
 };
 
 
 // stage 2
 let wildPoketmon2_1 = {
-    name: "Nidoking",
+    name: "happydancer",
     hp: 400,
-    skill_name: ["poison", "struggle", "hpUp", "Sheer"],
-    skill_damage: [-20, 0, 50, -37],
-    skill_point: [15, 15, 5, 15],
+    skill_name: ["Dance", "Shear", "HpUp", "HeadSpin"],
+    skill_damage: [-20, -20, 77, -37],
+    skill_point: [100, 100, 100, 100],
 };
 let wildPoketmon2_2 = {
-    name: "Nidoqueen",
+    name: "hellohola",
     hp: 300,
-    skill_name: ["poison", "struggle", "hpUp", "Sheer"],
-    skill_damage: [-20, 0, 50, -37],
-    skill_point: [15, 15, 5, 15],
+    skill_name: ["Greeting", "Handshaking", "HpUp", "Spiting"],
+    skill_damage: [-5, -20, 177, -15],
+    skill_point: [100, 100, 100, 100],
 };
 
 // stage 3
 let wildPoketmon3_1 = {
-    name: "Yadon",
+    name: "picky",
     hp: 400,
-    skill_name: ["ya", "do", "on", "hpUp"],
-    skill_damage: [-10, -15, -20, 50],
-    skill_point: [15, 15, 15, 5],
+    skill_name: ["FirstTeeth", "SecondTeeth", "ThirdTeeth", "FourthTeeth"],
+    skill_damage: [-10, -20, -35, -55],
+    skill_point: [100, 100, 100, 100],
 };
 let wildPoketmon3_2 = {
-    name: "Yadoran",
+    name: "monsterJelly",
     hp: 500,
-    skill_name: ["ya", "do", "ran", "hpUp"], 
-    kill_damage: [-10, -15, -20, 100],
-    skill_point: [15, 15, 15, 5],
+    skill_name: ["Jellyfy", "Rage", "Inrage", "hpUp"], 
+    kill_damage: [-100, -120, -210, 100],
+    skill_point: [100, 100, 100, 100],
 };
 let wildPoketmon3_3 = {
-    name: "MegaYadoran  ",
+    name: "unluckyangel",
     hp: 1000,
-    skill_name: ["mega", "ya", "do", "ran"],
-    kill_damage: [-10, -15, -20, -200],
-    skill_point: [15, 15, 15, 5],
+    skill_name: ["Somersault", "LoveDive", "Stare", "HpUp"],
+    kill_damage: [-100, -150, -20, 500],
+    skill_point: [100, 100, 100, 100],
 };
 
 // stage 4
 let wildPoketmon4_1 = {
-    name: "JamManBo",
-    hp: 1500,
-    skill_name: ["Zzz", "Bite", "Momtong", "Hapum"],
-    skill_damage: [100, -100, -200, -15],
-    skill_point: [10, 10, 5, 25],
+    name: "sleepy",
+    hp: 3000,
+    skill_name: ["Zzz", "CocoNenne", "HpUp", "Hapum"],
+    skill_damage: [1000, 700, 800, -200],
+    skill_point: [100, 100, 100, 100],
 };
 let wildPoketmon4_2 = {
-    name: "Majayong",
-    hp: 700,
-    skill_name: ["hpUp", "Mirror", "Momtong", "Majayong"],
-    skill_damage: [300, 0, -5, -30],
-    skill_point: [10, 10, 20, 15],
+    name: "tboy",
+    hp: 1300,
+    skill_name: ["HpUp", "Mirror", "Meteo", "Hacking"],
+    skill_damage: [300, 10, -444, -139],
+    skill_point: [100, 100, 100, 100],
 };
 let wildPoketmon4_3 = {
-    name: "Happynurse",
-    hp: 800,
-    skill_name: ["hpUp1", "hpUp2", "hpUp3", "melody"],
-    skill_damage: [150, 175, 200, -37],
-    skill_point: [7, 7, 7, 35],
+    name: "cornelio",
+    hp: 1800,
+    skill_name: ["ThrowPickle", "Crying", "HpUp", "Boxing"],
+    skill_damage: [-150, -175, 200, -78],
+    skill_point: [100, 100, 100, 100],
 };
 let wildPoketmon4_4 = {
-    name: "Dialga",
+    name: "demon",
     hp: 1717,
-    skill_name: ["breath", "mist", "flying", "hpUp"],
-    skill_damage: [-100, -75, -66, 757],
-    skill_point: [15, 10, 10, 7],
+    skill_name: ["Curse", "Spawn", "FireRain", "Psychic"],
+    skill_damage: [-200, -33, -66, -666],
+    skill_point: [100, 100, 100, 100],
 };
-
-let wildPoketArr = {};
-wildPoketArr[0] =  wildPoketmon1;
-wildPoketArr[1] = {};
-wildPoketArr[1][0] = wildPoketmon2_1;
-wildPoketArr[1][1] = wildPoketmon2_2;
-wildPoketArr[2] = {};
-wildPoketArr[2][0] = wildPoketmon3_1;
-wildPoketArr[2][1] = wildPoketmon3_2;
-wildPoketArr[2][2] = wildPoketmon3_3;
-wildPoketArr[3] = {};
-wildPoketArr[3][0] = wildPoketmon4_1;
-wildPoketArr[3][1] = wildPoketmon4_2;
-wildPoketArr[3][2] = wildPoketmon4_3;
-wildPoketArr[3][3] = wildPoketmon4_4;
-
-export { myPoketArr, wildPoketArr };
